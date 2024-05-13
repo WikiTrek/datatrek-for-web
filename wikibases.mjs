1f7f288c5e777a00c4dafb0ca4ee1da1900f5880
Luca Mauri <lucamauri@users.noreply.github.com>
2024-05-13 11:01:41 +0200

    Update wikibases.mjs
    Reference to trekSites object from the trekSites.mjs file

--- wikibases.mjs
+++ wikibases.mjs
@@ -1,5 +1,6 @@
 import { WBK } from './importmap/wikibase-sdk/dist/src/wikibase-sdk.js';
 import wikidataSites from './wikidataSites.mjs';
+import trekSites  from './trekSites.mjs';
 
 async function fetchInterwikiMap(api) {
 	// Construct the full API endpoint URL
