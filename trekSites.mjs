aa69a0a1a6f069d1c5fedd672cbebe566a6de3f2
Alex <blank@Mac.fritz.box>
2025-04-06 19:01:07 +0200

    rebase + linkfix + icon

--- trekSites.mjs
+++ trekSites.mjs
@@ -1,14 +1,22 @@
 const trekSites = {
-  datatrek: { language: 'it', pagePath: 'https://data.wikitrek.org/wiki/$1' },
-  dema: { language: 'de', pagePath: 'https://memory-alpha.fandom.com/de/$1' },
-  demb: { language: 'de', pagePath: 'https://memory-beta.fandom.com/de/$1' },
+  dema: {
+    language: 'de',
+    pagePath: 'https://memory-alpha.fandom.com/de/wiki/$1',
+  },
+  demb: {
+    language: 'de',
+    pagePath: 'https://memory-beta.fandom.com/de/wiki/$1',
+  },
   enma: { language: 'en', pagePath: 'https://memory-alpha.fandom.com/wiki/$1' },
   enmb: { language: 'en', pagePath: 'https://memory-beta.fandom.com/wiki/$1' },
-  itma: { language: 'it', pagePath: 'https://memory-alpha.fandom.com/it/wiki/$1' },
+  itma: {
+    language: 'it',
+    pagePath: 'https://memory-alpha.fandom.com/it/wiki/$1',
+  },
   fanlore: { language: 'en', pagePath: 'https://fanlore.org/w/$1' },
   sto: { language: 'en', pagePath: 'https://stowiki.net/wiki/$1' },
   trekipedia: { language: 'en', pagePath: 'http://trekipedia.com/mw/$1' },
   wikitrek: { language: 'it', pagePath: 'https://wikitrek.org/wiki/$1' },
-}
+};
 
 export default trekSites;
